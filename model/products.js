7fecb1b099b23a549c0774c716dae626d5faaa17
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-18 23:55:18 +0530

    strictQuery arg in Schema constructor to throw error in case query properties doesnot match schema properties

--- model/products.js
+++ model/products.js
@@ -1,33 +1,37 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "product name required"],
-  },
-  price: {
-    type: Number,
-    required: [true, "product price required"],
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-  rating: {
-    type: Number,
-    default: 3,
-  },
-  createAt: {
-    type: String,
-    default: new Date().toLocaleString(),
-  },
-  company: {
-    type: String,
-    enum: {
-      values: ["ikea", "liddy", "caressa", "marcos"],
-      message: "{VALUE} is not supported",
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "product name required"],
+    },
+    price: {
+      type: Number,
+      required: [true, "product price required"],
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    rating: {
+      type: Number,
+      default: 3,
+    },
+    createAt: {
+      type: String,
+      default: new Date().toLocaleString(),
+    },
+    company: {
+      type: String,
+      enum: {
+        values: ["ikea", "liddy", "caressa", "marcos"],
+        message: "{VALUE} is not supported",
+      },
     },
   },
-});
+  { strictQuery: "throw"}
+  // { strictQuery: false}
+);
 
-module.exports = mongoose.model('ProductsCollection', productSchema);
+module.exports = mongoose.model("ProductsCollection", productSchema);
