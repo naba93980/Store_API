dc80d7b7f99b6dd870572719d6a8abc461c76e9f
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-18 16:12:19 +0530

    document schema

---
+++ model/products.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "product name required"],
+  },
+  price: {
+    type: Number,
+    required: [true, "product price required"],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 3,
+  },
+  createAt: {
+    type: String,
+    default: new Date().toLocaleString(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported",
+    },
+  },
+});
+
+module.exports = mongoose.model('ProductsCollection', productSchema);
