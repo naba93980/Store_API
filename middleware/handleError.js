fa059eec2835a26ce65523fb7a24778eeeffab79
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-18 23:51:11 +0530

    updated error message

--- middleware/handleError.js
+++ middleware/handleError.js
@@ -1,5 +1,5 @@
 const errorHandler = (error, req, res, next) => {
-  res.status(500).json({ mssg: 'something went wrong' });
+  res.status(500).json({ mssg: error.message });
 };
 
 module.exports = errorHandler;
