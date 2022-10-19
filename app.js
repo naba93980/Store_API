8c15165487120c79b6c5e8c19ee077af00427e74
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-19 22:12:01 +0530

    added router, errorhandler,not found middleware, db connection etc

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const env = require("dotenv");
-env.config();
-const PORT = process.env.PORT || 5000;
-
-const express = require("express");
-const app = express();
-
+require("express-async-errors");
+const connectDB = require('./db/connect');
 const notFound = require("./middleware/notFound");
 const errorHandler = require("./middleware/handleError");
+const env = require("dotenv");
+env.config();
+const router = require('./routes/products');
+const PORT = process.env.PORT || 5000;
+const URI = process.env.MONGO_URI;
+const express = require("express");
+const app = express();
 
 app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
-app.get("/api/v1/products");
+app.use("/api/v1/products",router);
 
 app.use(notFound);
 app.use(errorHandler);
 
 const start = async () => {
   try {
-    //connect DB
+    await connectDB(URI);
     app.listen(PORT, () => console.log(`server started at port ${PORT}`));
   } catch (err) {
     console.log(err);
   }
 };
+
 start();
